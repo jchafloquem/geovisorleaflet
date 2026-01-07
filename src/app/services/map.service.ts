5b97c0420affb8a68598d1b4b8ecce58aeec3e4e
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2026-01-07 09:59:13 -0500

    Mejorar mapa base

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -16,13 +16,15 @@ export class MapService {
     }
 
     this.map = L.map(elementId, {
-      center: [51.505, -0.09], // Coordenadas iniciales de ejemplo
-      zoom: 13
+      center: [-12.05, -77.05], // Coordenadas iniciales de ejemplo
+      zoom: 15,
+      zoomControl: false
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    const tiles = L.tileLayer('https://{s}.google.com/vt/lyrs=s&hl=es&x={x}&y={y}&z={z}', {
+      maxZoom: 24,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      attribution: '&copy; <a href="https://www.google.com/permissions/geoguidelines/">Google Maps</a>'
     });
 
     tiles.addTo(this.map);
